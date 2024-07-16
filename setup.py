8ed57b66ce0a2e7f27e5e91a7d5c6603f466e245
Elchin523 <mustafayev.elchin@gmail.com>
2024-07-16 22:42:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='IPFInitFit',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     install_requires=[
         'numpy',
